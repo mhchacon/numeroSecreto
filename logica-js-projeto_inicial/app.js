af8beea87c6dae295bec5e99d2ce5c40d4b8e92d
mhchacon <mariana.chacon@ufrpe.br>
2024-01-23 13:23:33 -0300

    first commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,27 @@
+alert("Boa vindas ao jogo do número secreto");
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo+ 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e " + numeroMaximo);
+    
+    if (chute == numeroSecreto){
+        
+        break;
+    } else{
+        if(chute > numeroSecreto){
+            alert("o número secreto é menor que " + chute);
+        }else{
+            alert("o número secreto é maior que " + chute);
+        }
+        tentativas ++;
+    }
+     
+}
+
+let palavraTentativa = tentativas > 1 ? " tentativas" : " tentativa";
+alert("Isso ai! você acertou o número secreto " + numeroSecreto + " com " + tentativas + palavraTentativa);
+
